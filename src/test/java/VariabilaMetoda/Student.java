f2cb36c00817187a4c6998ff585ae352ae9d6711
Krisztina Kacsur <Krisztina.Kacsur@endava.com>
2024-11-28 15:42:58 +0200

    first commit

---
+++ src/test/java/VariabilaMetoda/Student.java
@@ -0,0 +1,62 @@
+package VariabilaMetoda;
+
+import com.google.errorprone.annotations.Var;
+import org.testng.annotations.Test;
+
+public class Student {
+
+    //clasa = sablon care defineste proprietatile si comportamentul unei antitati specifice
+    //intr-un fisier Java putem avea mai multe clase diferentiate pe baza numelui
+    //nu este un practice bun sa ai mai multe clase cu acelasi nume
+    //intr-o clasa putem defini proprietatile si actiunile entitatii
+    //variabila = proprietatea unei clase
+    // variabilele pot fi de 2 feluri: global si local
+    //variabila globala = sunt vizibile peste tot in proiect
+    //variabila locala = sunt vizibile unde sunt declarate
+    // ca sa definim a variabila globala: accesscontrol (public) | tip variabila | nume variabila
+    //cand definim o variabila nu este obligatoriu sa ii dam o valoare
+    //metoda = actiunea unei clase
+    // 2 tipuri de metode: void, returned
+
+    public String Nume;
+    public String Prenume;
+    public Integer Varsta;
+    public String Adresa;
+    public Double Inaltime;
+    public Float Greutate;
+    public Character Sex;
+    public Boolean AreDiploma;
+
+    @Test
+    public void prezentareStudent (){
+        Nume = "Krisz";
+        Prenume = "Kri";
+        Varsta = 28;
+        Adresa = "str Fericirii nr 42";
+        Inaltime = 1.58;
+        Greutate = 50.26f;
+        Sex = 'F';
+        AreDiploma = true;
+
+        //System.out.println(Nume+" "+Prenume+ " " +Varsta+ " " + Adresa);
+        //System.out.println(Inaltime);
+        System.out.println("Numele studentului este: " + Nume);
+        System.out.println("Prenume studentului este: " + Prenume);
+        System.out.println("Varsta studentului este: " + Varsta);
+        System.out.println("Adresa studentului este: " + Adresa);
+        System.out.println("Inaltimea studentului este: " + Inaltime);
+        System.out.println("Greutatea studentului este: " + Greutate);
+        System.out.println("Sexul studentului este: " + Sex);
+        System.out.println("Are studentul diploma? " + AreDiploma);
+
+        calculMedie();
+    }
+
+    public void calculMedie(){
+        Integer notaUnu = 8;
+        Integer notaDoi = 9;
+        Integer notaTrei = 4;
+        Integer medie = (notaUnu + notaDoi + notaTrei) / 3;
+        System.out.println("Nota studentului este: " + medie);
+    }
+}
