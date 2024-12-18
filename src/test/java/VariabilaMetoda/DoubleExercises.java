cc6ee06e53a79b16bce504b4997204095129edeb
KrisztinaKacsur <kricsk3@gmail.com>
2024-12-18 09:40:23 +0200

    second commit

---
+++ src/test/java/VariabilaMetoda/DoubleExercises.java
@@ -0,0 +1,46 @@
+package VariabilaMetoda;
+
+import org.testng.annotations.Test;
+
+public class DoubleExercises {
+
+    @Test
+    public void calcTest(){
+        volumeOfCylinderCalc(125.3, 89.7);
+        fahrenheitToC(91.3);
+        hypotenuseCalc(56.7, 58.3);
+    }
+
+
+        /*Calculate the Volume of a Cylinder
+        Initialize two doubles radius and height for a cylinder.
+        Calculate and print the volume of the cylinder (use Math.PI for π).*/
+
+    public void volumeOfCylinderCalc(Double paramHeight, Double paramRadius){
+        double volume = Math.PI * (paramHeight * paramRadius) * paramHeight;
+        System.out.println("The volume of the Cylinder is: " + volume);
+    }
+
+
+        /*Convert Fahrenheit to Celsius
+        Initialize a double Fahrenheit with any value. Convert this to Celsius and print the result.
+        C = (°F - 32) × 5/9*/
+
+    public void fahrenheitToC (Double paramFahrenheit){
+        double celsius = (paramFahrenheit -32 ) * 5 / 9;
+        System.out.println(paramFahrenheit + " Fahrenheits = " + celsius + " C");
+
+    }
+
+
+        /*Calculate the Hypotenuse of a Right-Angled Triangle
+        Given the lengths of the two shorter sides of a right-angled triangle, calculate the length of the hypotenuse.
+        Use Pythagoras theorem.*/
+
+    public void hypotenuseCalc (Double paramSideA, Double paramSideB){
+        double hypotenuse = Math.sqrt(Math.pow(paramSideA,2) + Math.pow(paramSideB,2));
+        System.out.println("The hypotenuse of the  rightAngledTriangle: " + hypotenuse + " cm");
+
+    }
+
+}
---
+++ src/test/java/VariabilaMetoda/IntExercises.java
@@ -0,0 +1,44 @@
+package VariabilaMetoda;
+
+import org.testng.annotations.Test;
+
+public class IntExercises {
+
+    @Test
+    public void calcTest(){
+        rectanglePerimeterCalc(364,250);
+        minToSecCalc(13);
+        areaOfSquareCalc(6);
+    }
+
+
+        /*Calculate the Perimeter of a Rectangle:
+        Initialize two integers length and width with any values.
+        Calculate and print the perimeter of a rectangle using these variables. */
+
+    public void rectanglePerimeterCalc(Integer paramRectangleLength, Integer paramRectangleWidth){
+        Integer rectanglePerimeter = 2 * (paramRectangleLength + paramRectangleWidth);
+        System.out.println("The perimeter of the rectangle is: " + rectanglePerimeter + " cm");
+    }
+
+
+        /*Convert Minutes into Seconds:
+        Initialize an integer minutes with any value.
+        Convert this into seconds and print the result.*/
+
+    public void minToSecCalc(Integer paramMinute){
+        Integer minToSecResult = paramMinute * 60;
+        System.out.println(paramMinute + " minutes = " + minToSecResult + " seconds");
+    }
+
+
+        /*Calculate the Area of a Square
+        Initialize an integer side representing the side of a square.
+        Calculate and print the area of the square.*/
+
+    public void areaOfSquareCalc(Integer paramSquareSide){
+        Integer area = paramSquareSide * paramSquareSide;
+        System.out.println("The area of the square is: " + area + " cm");
+    }
+
+}
